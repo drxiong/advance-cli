3832ad8ecba9b0aa1775940f723ac4d901115b58
@yan.xiong <1763303455@qq.com>
2022-05-15 12:31:08 +0800

    feat: advance-cli大致完成

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'standard', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {}
+}
+
